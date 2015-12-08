b938bb8fbcec8e55c30865eaa7574f9f8784d102
MattWise <ttam.esiw@gmail.com>
2015-12-08 07:15:51 -0500

    Implemented 4 and 5c. Run time for the entire project is ~3 minutes

--- Yahtzee.py
+++ Yahtzee.py
@@ -42,9 +42,16 @@ def problem3(N=(0,1,2,3)):
         print("\n{} rerolls:".format(n))
         a.prnt()
 
-def problem5b():
+def problem4(category="aces",rerolls=2):
     p=allRolls()
-    randomOutcomes=calculateOutcomesRandom(Rules["chance"][0],p,rerolls=0)
+    print("\nProblem 4: Expected Value and Variance in score of {}\n".format(category))
+    outcomes=calculateOutcomes(Rules[category][0],p,rerolls)
+    a=AnalyzeRandomVariable(Rules[category][1],outcomes)
+    a.prnt()
+
+def problem5b(times=10000):
+    p=allRolls()
+    randomOutcomes=calculateOutcomesRandom(Rules["chance"][0],p,rerolls=0,times=times)
     outcomes=calculateOutcomes(Rules["chance"][0],p,0)
     a=AnalyzeRandomVariable(Rules["chance"][1],outcomes)
     randomA=AnalyzeRandomVariable(Rules["chance"][1],randomOutcomes)
@@ -54,7 +61,22 @@ def problem5b():
     print("\nRandom:")
     randomA.prnt()
 
+def problem5c(category="aces",rerolls=2,times=10000):
+    p=allRolls()
+    print("\nProblem 5c: Expected Value and Variance in score of {}\n".format(category))
+    outcomes=calculateOutcomes(Rules[category][0],p,rerolls)
+    a=AnalyzeRandomVariable(Rules[category][1],outcomes)
+    print("\nAnalytic:")
+    a.prnt()
+    randomOutcomes=calculateOutcomesRandom(Rules[category][0],p,rerolls,times)
+    randomA=AnalyzeRandomVariable(Rules[category][1],randomOutcomes)
+    print("\nRandom:")
+    randomA.prnt()
+
+
 
 problem1()
 problem3()
+problem4()
 problem5b()
+problem5c()
